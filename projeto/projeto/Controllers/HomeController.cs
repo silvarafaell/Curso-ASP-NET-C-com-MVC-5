ce9eebe00291951111203e3d99ce1969817b0c0a
Fitbank <francisco.silva@fitbank.com.br>
2020-02-04 10:10:54 -0300

    Aula 20 passando valores com POST

--- projeto/projeto/Controllers/HomeController.cs
+++ projeto/projeto/Controllers/HomeController.cs
@@ -25,8 +25,11 @@ namespace projeto.Controllers
         }
 
         [HttpPost]
-        public ActionResult Lista()
+        public ActionResult Lista(int PessoaId, string Nome, string Tipo)
         {
+            ViewData["PessoaId"] = PessoaId;
+            ViewData["Nome"] = Nome;
+            ViewData["Tipo"] = Tipo;
             return View();
         }
 
