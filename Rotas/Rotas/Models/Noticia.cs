f0b6e1e667da8f383bf91af99460eab7000958ff
Fitbank <francisco.silva@fitbank.com.br>
2020-02-05 13:50:36 -0300

    Aula 38 - Regra de validação para cadastros duplicados e inicio de Rota

---
+++ Rotas/Rotas/Models/Noticia.cs
@@ -0,0 +1,10 @@
+﻿namespace Rotas.Models
+{
+    public class Noticia
+    {
+        public int NoticiaId { get; set; }
+        public string Titulo { get; set; }
+        public string Categoria { get; set; }
+        public string Data { get; set; }
+    }
+}
