9ab424d197f0fc72ed0c69a175fafedb31a5626c
Fitbank <francisco.silva@fitbank.com.br>
2020-02-05 14:33:35 -0300

    Aula 39 - Criando uma classe para noticias

--- Rotas/Rotas/Controllers/HomeController.cs
+++ Rotas/Rotas/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Rotas.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,12 @@ namespace Rotas.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IEnumerable<Noticia> todasAsNoticias;
+
+        public HomeController()
+        {
+
+        }
         public ActionResult Index()
         {
             return View();
