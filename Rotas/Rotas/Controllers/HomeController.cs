63d7d4c1b8590cd45c95bb6c1cfad3005c6a06a9
Fitbank <francisco.silva@fitbank.com.br>
2020-02-06 11:49:48 -0300

    Aula 43 - Listando as categorias

--- Rotas/Rotas/Controllers/HomeController.cs
+++ Rotas/Rotas/Controllers/HomeController.cs
@@ -24,19 +24,6 @@ namespace Rotas.Controllers
 
             return View(ultimasNoticias);
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+       
     }
 }
